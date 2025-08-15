5afc138a4908e278ee2be042dda4174ac40d42f3
donovanjb <donovanjb03@gmail.com>
2025-08-14 23:09:13 -0400

    Updated file organization

---
+++ godot-rl-agents/addons/godot_rl_agents/onnx/csharp/ONNXInference.cs
@@ -0,0 +1,14 @@
+using Godot;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GodotONNX
+{
+    public partial class ONNXInference : GodotObject
+    {
+        // Removed Export attribute from Initialize method
+    }
+}
